96005f3a4e5f7c1c640b2f217af008bff0141b2b
Stav Yaar Bar <anistu@gmail.com>
2022-05-31 16:30:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,127 @@
+'use strict'
+
+var isStop = true
+
+function stop() {
+    isStop = true
+}
+
+
+
+function addToQueue(...terms) {
+    var elPlayListContainer = document.querySelector('#player-container')
+    if (elPlayListContainer?.children.length) return
+    
+    chrome.runtime.sendMessage({ type: 'queue', isRunningQueue: true })
+    const isSearchKeyInclude = (string, searchKey) => {
+        if (!searchKey) return true
+        // * comment next line for normal filter
+        // searchKey = '.*' + searchKey.split('').join('.*') + '.*' 
+        const isInclude = new RegExp(searchKey, 'i')
+        return isInclude.test(string)
+    }
+
+    let els = document.querySelectorAll("#items > ytd-grid-video-renderer")
+    els = [...els].reverse()
+    els.forEach(el => {
+        const title = el.querySelector('#details #meta #video-title').innerText
+        const isIncludes = terms.some(term => isSearchKeyInclude(title, term))
+        if (!isIncludes) return
+        const mouseenterEvent = new Event('mouseenter');
+        el.dispatchEvent(mouseenterEvent);
+        var elAddToQueue = el.querySelector('#hover-overlays > ytd-thumbnail-overlay-toggle-button-renderer:nth-child(2) #icon')
+        elAddToQueue?.click()
+        const mouseleaveEvent = new Event('mouseleave');
+        el.dispatchEvent(mouseleaveEvent);
+    })
+    chrome.runtime.sendMessage({ type: 'queue', isRunningQueue: false })
+
+}
+
+
+
+
+function scrollToTime(amount, timePeriod, page = 0) {
+    isStop = false
+
+    // toggle is running
+
+    const onToggleIsRunning = (isRunningScroll) => chrome.runtime.sendMessage({ isRunningScroll, type: 'scroll' })
+
+    const timesValMap = {
+        day: 0,
+        days: 0,
+        week: 1,
+        weeks: 1,
+        month: 2,
+        months: 2,
+        year: 3,
+        years: 3,
+    }
+
+    let lastLength = 0
+    let sameLengthCount = 0
+    function innerRecursive(amount, timePeriod, page) {
+
+        if (isStop) return
+        timePeriod = timePeriod.toLocaleLowerCase()
+        amount = +amount
+        let elSpans = document.querySelectorAll("#metadata-line > span:nth-child(2)")
+        elSpans = [...elSpans]
+        if (elSpans.length === lastLength) {
+            sameLengthCount++
+            if (sameLengthCount > 150) return onToggleIsRunning(false)
+        } else {
+            sameLengthCount = 0
+        }
+        lastLength = elSpans.length
+
+        if (amount === 1) {
+            if (timePeriod.endsWith('s')) timePeriod = timePeriod.slice(0, -1)
+        } else if (amount > 1) {
+            if (!timePeriod.endsWith('s')) timePeriod += 's'
+        } else return onToggleIsRunning(false)
+        var spans = [elSpans.at(-20), elSpans.at(-1)]
+        for (const elSpan of spans) {
+            if (!elSpan) continue
+            let parts = elSpan?.innerText?.split(' ')
+            let spanAmount = parts.at(-3)
+            let spanTimePeriod = parts.at(-2)
+            spanAmount = +spanAmount
+            if (timesValMap[spanTimePeriod] > timesValMap[timePeriod]) return onToggleIsRunning(false)
+            if (timesValMap[spanTimePeriod] === timesValMap[timePeriod] && spanAmount >= amount) return onToggleIsRunning(false)
+        }
+
+
+        window.scrollTo(0, page * 10000 + window.scrollY + 10000)
+        setTimeout(() => {
+            innerRecursive(amount, timePeriod, page + 1)
+        }, 0);
+
+    }
+
+    window.scrollTo(0, page * 10000 + window.scrollY + 1000)
+    innerRecursive(amount, timePeriod, page)
+
+}
+
+
+
+function scrollToBottom(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        setTimeout(((i) => {
+            return () => window.scrollTo(0, i * 10000 + window.scrollY + 1000);
+        })(i), 0 * i, i);
+    }
+}
+
+
+// let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+// chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: fillInputs,
+//     args: [startingDay, endDay, clockInHour, clockOutHour]
+// });
+
+
---
+++ popup.js
@@ -0,0 +1,93 @@
+'use strict'
+
+window.addEventListener('DOMContentLoaded', () => {
+    onInit();
+})
+
+let gElAddToQBtn
+let gElLoadVideosBtn
+let gElStopBtn
+let gIsRunning = false
+
+function onInit() {
+    gElAddToQBtn = document.querySelector('.add-to-q')
+    gElLoadVideosBtn = document.querySelector('.load-videos-btn')
+    gElStopBtn = document.querySelector('.stop-btn')
+    addEventListeners()
+    chrome.runtime.onMessage.addListener(({ type, isRunningScroll }) => {
+        if (type === 'queue') {
+            onToggleImgLoader()
+        } else if (type === 'scroll') {
+            onChangeStopBtnTxt(isRunningScroll)
+        }
+    })
+
+}
+
+
+function shakeBtn() {
+    gElAddToQBtn.classList.add('shake-animation')
+    setTimeout(() => {
+        gElAddToQBtn.classList.remove('shake-animation')
+    }, 500);
+}
+
+
+async function onAddToQueue({ target }) {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const elTermInput = document.querySelector('[name="search-term"]')
+    const term = elTermInput.value
+    if (!term) return shakeBtn(target)
+    let terms = term.split(',').map(term => term.trim())
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: addToQueue,
+        args: [...terms]
+    });
+}
+
+async function onToggleLoadVideos(ev) {
+
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    const elTimeAmount = document.querySelector('.time-amount')
+    const elPeriod = document.querySelector('select[name="period"]')
+    const period = elPeriod.value
+    let amount = +elTimeAmount.value
+    if (!amount) amount = 1
+    const func = gIsRunning ? stop : scrollToTime
+    gIsRunning = !gIsRunning
+    onChangeStopBtnTxt(gIsRunning)
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: func,
+        args: [amount, period]
+    });
+}
+
+
+async function onStop() {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: stop,
+        args: []
+    });
+}
+
+
+function addEventListeners() {
+    gElAddToQBtn.addEventListener('click', onAddToQueue);
+    gElLoadVideosBtn.addEventListener('click', onToggleLoadVideos);
+}
+
+function onChangeStopBtnTxt(isRunning) {
+    gIsRunning = isRunning
+    const txt = isRunning ? 'Stop' : 'Load Videos'
+    gElLoadVideosBtn.innerText = txt
+
+}
+
+function onToggleImgLoader() {
+    gElAddToQBtn.querySelector('img').classList.toggle('hide')
+    gElAddToQBtn.querySelector('span').classList.toggle('hide')
+}
