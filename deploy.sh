e56c2bccc71e0b6edb8b6baf36e916d7a67decb8
Stav Yaar Bar <anistu@gmail.com>
2025-04-17 22:57:46 +0300

    minor css

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,30 @@
 #!/bin/zsh
 
-# Get the version from manifest.json
+#* Get the version from manifest.json
 version=$(grep '"version"' manifest.json | cut -d'"' -f4)
 
-# Remove dots from version to use in folder name
+#* Remove dots from version to use in folder name
 version_no_dots=$(echo $version | tr -d '.')
 
-# Create folder name
+#* Create folder name
 folder_name="create-youtube-playlist-v$version_no_dots"
 
-# Get current directory name
+#* Get current directory name
 current_dir=${PWD##*/}
 
-# Create deploy directory in parent folder if it doesn't exist
+#* Create deploy directory in parent folder if it doesn't exist
 mkdir -p "../deploy"
 
-# Create temporary directory for the copy
+#* Create temporary directory for the copy
 deploy_path="../deploy/$folder_name"
 
-# Remove existing deploy folder if it exists
+#* Remove existing deploy folder if it exists
 rm -rf "$deploy_path"
 
-# Copy all files except .git directory
+#* Copy all files except .git directory
 rsync -av --exclude '.git' --exclude 'deploy.sh' . "$deploy_path"
 
-# Create zip file
+#* Create zip file
 cd "../deploy"
 zip -r "${folder_name}.zip" "$folder_name"
 cd "../$current_dir"
